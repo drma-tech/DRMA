318eeceff88b2999c11054fce25d7e04038523f1
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-03-31 18:31:52 -0300

    appinfo class

---
+++ DRMA.WEB/Core/AppInfo.cs
@@ -0,0 +1,20 @@
+﻿using DRMA.WEB.Modules.Support.Core;
+
+namespace DRMA.WEB.Core
+{
+    public static class AppInfo
+    {
+        public static string CompanyName { get; set; } = "DRMA Tech";
+        public static string CompanyWebSite { get; set; } = $"https://www.drma-tech.com";
+
+        public static int Year { get; set; } = 2025;
+
+        public static readonly ProductLink[] Products =
+        [
+            new("Streaming Discovery", "Manage, Track, Discover", "https://www.streamingdiscovery.com", "/logo/streamingdiscovery.png", true ),
+            new("Modern Matchmaker", "Find your life partner", "https://www.modern-matchmaker.com", "/logo/modern-matchmaker.png", true ),
+            new("My Next Spot", "Match your next destination", "https://www.my-next-spot.com", "/logo/next-spot.png", false ),
+            new("WebStandards", "Web Developer Tools", "https://www.web-standards.com/", "/logo/webstandards.png", false ),
+       ];
+    }
+}
