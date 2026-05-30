57a57c1869440c34ea88f393bc0214b8232da232
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-05-30 18:15:52 -0300

    ...

--- DRMA.WEB/Core/AppInfo.cs
+++ DRMA.WEB/Core/AppInfo.cs
@@ -15,10 +15,10 @@ namespace DRMA.WEB.Core
 
         public static readonly ProductLink[] Products =
         [
-            new("Streaming Discovery", "Manage, Track, Discover", "https://www.streamingdiscovery.com", "/logo/streamingdiscovery.png", true ),
-            new("Modern Matchmaker", "Find your life partner", "https://www.modern-matchmaker.com", "/logo/modern-matchmaker.png", true ),
-            new("My Next Spot", "Match your next destination", "https://www.my-next-spot.com", "/logo/next-spot.png", false ),
-            new("WebStandards", "Web Developer Tools", "https://www.web-standards.com", "/logo/webstandards.png", false ),
+            new("Streaming Discovery", "Discover Movies and Series on Streaming Platforms", "https://www.streamingdiscovery.com", "/logo/streamingdiscovery.png", true ),
+            new("Modern Matchmaker", "Find a Compatible Partner Through Smart Matching", "https://www.modern-matchmaker.com", "/logo/modern-matchmaker.png", true ),
+            new("My Next Spot", "Find the Best Cities and Countries to Live or Travel", "https://www.my-next-spot.com", "/logo/next-spot.png", false ),
+            new("WebStandards", "Web Standards Generator for Websites and PWAs", "https://www.web-standards.com", "/logo/webstandards.png", false ),
        ];
     }
 }
