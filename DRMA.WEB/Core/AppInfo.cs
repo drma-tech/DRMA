42f57dfcc87f7fec11820373fd481e9c783131a3
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-06-05 17:01:19 -0300

    code review

--- DRMA.WEB/Core/AppInfo.cs
+++ DRMA.WEB/Core/AppInfo.cs
@@ -16,9 +16,9 @@ namespace DRMA.WEB.Core
         public static readonly ProductLink[] Products =
         [
             new("Streaming Discovery", "Discover Movies and Series on Streaming Platforms", "https://www.streamingdiscovery.com", "/logo/streamingdiscovery.webp", true ),
-            new("Modern Matchmaker", "Find a Compatible Partner Through Smart Matching", "https://www.modern-matchmaker.com", "/logo/modern-matchmaker.webp", true ),
-            new("My Next Spot", "Find the Best Cities and Countries to Live or Travel", "https://www.my-next-spot.com", "/logo/next-spot.webp", false ),
-            new("WebStandards", "Web Standards Generator for Websites and PWAs", "https://www.web-standards.com", "/logo/webstandards.webp", false ),
+            new("Modern Matchmaker", "Find a compatible partner through Smart Matchmaking", "https://www.modern-matchmaker.com", "/logo/modern-matchmaker.webp", true ),
+            new("My Next Spot", "Find the Best Cities and Countries to Live or Travel", "https://www.my-next-spot.com", "/logo/next-spot.webp", true ),
+            new("Web Standards", "Web Standards Generator for Websites and PWAs", "https://www.web-standards.com", "/logo/webstandards.webp", false ),
        ];
     }
 }
